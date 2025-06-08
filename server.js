a62b56074b851e82febc484f73deaf1ba45ece95
FAYnim <yuana6898>
2025-06-09 06:51:11 +0700

    Update server routes

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 
 const app = express();
 const PORT = 3000;
@@ -10,6 +11,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Route untuk halaman utama (index.html)
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/api/orders', (req, res) => {
+	try {
+		const order = fs.readFileSync(path.join(__dirname, 'data', 'orders.json'));
+		res.json(JSON.parse(order));
+	} catch {
+		res.status(500).json({'Failed to load order'});
+	}
 });
 
 // Jalankan server
